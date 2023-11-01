44f1bffd6ad7a11eb8229c21fa3886e729f5c5bb
GumaLee <rkdxhd97@naver.com>
2023-11-01 14:31:40 +0900

    Project Start

---
+++ src/test/java/capstone/doAds/DoAdsApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.doAds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
