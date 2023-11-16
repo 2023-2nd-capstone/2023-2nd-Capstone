5fe59a1f7dc797204e922724aaad4744390f8fb2
GumaLee <rkdxhd97@naver.com>
2023-11-16 18:29:07 +0900

    Refactor(ProfileController): 일반 Controller로 변경/ Feat: 인플루언서 프로필 페이지

--- src/main/java/capstone/doAds/controller/ProfileController.java
+++ src/main/java/capstone/doAds/controller/ProfileController.java
@@ -3,19 +3,21 @@ package capstone.doAds.controller;
 import capstone.doAds.dto.InfluencerProfileResponseDto;
 import capstone.doAds.service.ProfileService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ProfileController {
 
     private final ProfileService profileService;
 
     @GetMapping("/profile/{profile_id}")
-    public ResponseEntity<InfluencerProfileResponseDto> getInfluencerProfile(@PathVariable("profile_id") Long profileId) {
-        return ResponseEntity.ok(profileService.getInfluencerProfile(profileId));
+    public String getInfluencerProfile(@PathVariable("profile_id") Long profileId, Model model) {
+       InfluencerProfileResponseDto influencerProfileResponseDto = profileService.getInfluencerProfile(profileId);
+       model.addAttribute("profile", influencerProfileResponseDto);
+       return "influencerProfile";
     }
 }
